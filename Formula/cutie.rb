6d7329276544b44e7d2416b199f402dfe93e5ff9
Keir Lawson <keirlawson@gmail.com>
2026-07-01 15:02:48 +0100

    correct SHA

--- Formula/cutie.rb
+++ Formula/cutie.rb
@@ -1,7 +1,7 @@
 class Cutie < Formula
   homepage "https://github.com/keirlawson/cutie"
   url "https://github.com/keirlawson/cutie/releases/download/v0.1.1/cutie-macos-aarch64"
-  sha256 "369ee89d28fd7ba321c22fc2333645dfe0f1dd3487ca531341f7ac81cc98ff2e"
+  sha256 "a56ebf37b3f32696402077b19f2344195014b33c8e92b4de025f970322266b26"
 
   def install
     bin.install "cutie-macos-aarch64" => "cutie"
